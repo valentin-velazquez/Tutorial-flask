c679f45a8a0b0e23ca7df906baa2c6c55df828ae
Valentin Velazquez <valesergiovelazquez@gmail.com>
2025-03-26 11:33:34 -0300

    creamos un linl que te redirecciona a otra pagina

--- app.py
+++ app.py
@@ -4,4 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>pasta base, cocaina, merca!</p>"
+    return """
+           <a href= 'boca'>boca</a>
+           <a href= 'riBer'>riBer</a>
+    """
+    
+
+@app.route("/boca")
+def boquita():
+    return "<h1>boca el mas grande!</h1>"
+
+@app.route("/riBer")
+def riBer():
+    return "<h1>riBer equipo chico</h1>"
