200525290b2f1a6cfece4e9cab22a9b700478b34
Valentin Velazquez <valesergiovelazquez@gmail.com>
2025-04-09 10:57:50 -0300

    usamos el url para no tener que llamar a la direccion

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return """
-           <a href= '/HAMBURGUESA'>HAMBURGUESA</a>
-           <a href= '/papas_fritas'>papas_fritas</a>
-    """
+    
+    url_hamburguesa = url_for("COMIDA_condimentos", condimentos="cheddar")
+    url_papas = url_for("acompañamiento")
+    url_dados = url_for("dado", caras=6)
+    url_sumar = url_for("suma", n1=4, n2=9)
+    url_logo  =  url_for("static", filename="logo.png")
+    
+    return f"""
+       <a href='{url_hamburguesa}'>HAMBUERGUESA</a>
+       <a href='{url_papas}'>papas_fritas</a>
+       <a href='{url_dados}'>dados</a>
+       <a href='{url_sumar}'>sumar</a>
+       <a href='{url_logo}'>logo</a>
+
+"""
     
 
 @app.route("/HAMBUERGUESA")
 def COMIDA():
-    return "<h2>amo las hamburgeesas</h2>"
+    return "<h2>amo las hamburguesas</h2>"
 
 @app.route("/HAMBUERGUESA/<string:condimentos>")
 def COMIDA_condimentos(condimentos):
@@ -26,9 +37,12 @@ def acompañamiento():
 def dado(caras):
     from random import randint
     numero = randint(1,caras)
-    return "<h2>dado de {caras} caras, salio{numero}!</h2>"
+    return f"<h2>dado de {caras} caras, salio {numero}!</h2>"
 
 @app.route("/sumar/<int:n1>/<int:n2>")
 def suma(n1,n2):
     suma = n1+n2
     return f"<h2>{n1} mas {n2} da {suma}</h2>"
+
+
+
