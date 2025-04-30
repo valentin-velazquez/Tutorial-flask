2e53845291bb9e9b53edad6d565025676f1cb94b
Valentin Velazquez <valesergiovelazquez@gmail.com>
2025-04-30 12:07:05 -0300

    ejercicio insertar, mostrar y borrar. el update no terminado

--- app.py
+++ app.py
@@ -11,13 +11,23 @@ def hello_world():
     url_dados = url_for("dado", caras=6)
     url_sumar = url_for("suma", n1=4, n2=9)
     url_logo  =  url_for("static", filename="logo.png")
+    url_insertar = url_for("insertar", nombre = "valen", email = "valesergiovelazquez@gmail.com")
+    url_borrar = url_for("borrar", id = "2")
+    url_mostrar = url_for("mostrar", id = "1")
+
+    
     
     return f"""
-       <a href='{url_hamburguesa}'>HAMBUERGUESA</a>
-       <a href='{url_papas}'>papas_fritas</a>
-       <a href='{url_dados}'>dados</a>
-       <a href='{url_sumar}'>sumar</a>
-       <a href='{url_logo}'>logo</a>
+    <ul>
+       <li><a href='{url_hamburguesa}'>HAMBUERGUESA</a></li>
+       <li><a href='{url_papas}'>papas_fritas</a></li>
+       <li><a href='{url_dados}'>dados</a></li>
+       <li><a href='{url_sumar}'>sumar</a></li>
+       <li><a href='{url_logo}'>logo</a></li>
+       <li><a href='{url_insertar}'>insertar</a></li>
+       <li><a href='{url_borrar}'>borrar</a></li>
+       <li><a href='{url_mostrar}'>mostrar</a></li>
+       <ul>
 
 """
     
@@ -69,5 +79,91 @@ def obterGente():
     fila = [dict(row) for row in resultado]
 
     return fila             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+db = None
+
+
+def dict_factory(cursor, row):
+  """Arma un diccionario con los valores de la fila."""
+  fields = [column[0] for column in cursor.description]
+  return {key: value for key, value in zip(fields, row)}
+
+
+def abrirConexion():
+   global db
+   db = sqlite3.connect("instance/datos.sqlite")
+   db.row_factory = dict_factory
+
+
+def cerrarConexion():
+   global db
+   db.close()
+   db = None
+
+
+@app.route("/test-db")
+def testDB():
+   abrirConexion()
+   cursor = db.cursor()
+   cursor.execute("SELECT COUNT(*) AS cant FROM usuarios; ")
+   res = cursor.fetchone()
+   registros = res["cant"]
+   cerrarConexion()
+   return f"Hay {registros} registros en la tabla usuarios"
+
+
+
+@app.route("/insertar/<string:nombre>/<string:email>")
+def insertar(nombre, email):
+    abrirConexion()
+    db.execute("INSERT INTO usuarios(usuario, email) VALUES (?, ?);",(nombre, email))
+    db.commit()
+    cerrarConexion()
+    return f"se agregue el {nombre} y el email{email}"
+
+@app.route("/borrar/<string:id>")
+def borrar(id):
+    abrirConexion()
+    db.execute("DELETE FROM usuarios WHERE id = (?)",(id))
+    db.commit()
+    cerrarConexion()
+    return f"borraste el usuario con el id {id}"
+
+
+
+@app.route("/mostrar/<string:id>")
+def mostrar(id):
+   abrirConexion()
+   cursor = db.cursor()
+   cursor.execute("SELECT * FROM usuarios WHERE id = ?",(id))
+   usuario = cursor.fetchall()
+   cerrarConexion()
+   
+   if usuario:
+       return f"{usuario}"
+   else:
+       return f"no se encontro un usuario con id {id}"
+
+
+@app.route("/update/<string:id>")
+def update(id):
+    abrirConexion()
+    db.execute("DELETE FROM usuarios WHERE id = (?)",(id))
+    db.commit()
+    cerrarConexion()
+    return f"borraste el usuario con el id {id}"
 
 
